65ea0f8b9327053cb594f61afd2a7131d941d990
Zak <svetozariliev13@gmail.com>
2025-01-20 17:15:51 +0200

    add pages skeleton

---
+++ src/pages/login.js
@@ -0,0 +1,12 @@
+// import styles from "./login.module.css";
+
+export default function Login() {
+  return (
+    <div
+    //
+    // className={styles.page}
+    >
+      Login Page
+    </div>
+  );
+}
